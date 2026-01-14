517e4067f7042d30a8ecac2e581359b8f0ab3e01
wanderson-nascimento <wandersonnascimento94@gmail.com>
2026-01-14 14:15:15 -0300

    chore: update dependencie

--- src/node/node_modules/axios/lib/core/buildFullPath.js
+++ src/node/node_modules/axios/lib/core/buildFullPath.js
@@ -10,10 +10,13 @@ var combineURLs = require('../helpers/combineURLs');
  *
  * @param {string} baseURL The base URL
  * @param {string} requestedURL Absolute or relative URL to combine
+ * @param {boolean} allowAbsoluteUrls Set to true to allow absolute URLs
+ *
  * @returns {string} The combined full path
  */
-module.exports = function buildFullPath(baseURL, requestedURL) {
-  if (baseURL && !isAbsoluteURL(requestedURL)) {
+module.exports = function buildFullPath(baseURL, requestedURL, allowAbsoluteUrls) {
+  var isRelativeURL = !isAbsoluteURL(requestedURL);
+  if (baseURL && (isRelativeURL || allowAbsoluteUrls === false)) {
     return combineURLs(baseURL, requestedURL);
   }
   return requestedURL;
